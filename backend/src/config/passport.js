a90ce6ea87985ac6b40ce668b44f2797d3d919a9
skulldanger <benyadav443@gmail.com>
2026-07-15 08:01:55 +0530

    done

--- backend/src/config/passport.js
+++ backend/src/config/passport.js
@@ -6,7 +6,7 @@ const prisma = new PrismaClient();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:5000/auth/google/callback"
+   callbackURL: "https://ai-mock-interview-xnfn.onrender.com/api/auth/google/callback" 
 },
 async(accessToken,refreshToken,profile,done)=>{
     try{
